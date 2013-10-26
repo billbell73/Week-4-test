bbc90d6122c8b764602667c678ce9eac4af149f6
Will Bell <willbell@gmail.com>
2013-10-26 13:10:12 +0100

    Have got inject working, sorta

--- inject_method/lib/array.rb
+++ inject_method/lib/array.rb
@@ -52,9 +52,111 @@ class Array
 		accumulator
 	end
 
-	def takes_block_5
-
+	def takes_block_5 three, four
+		yield three,four
 	end
+
+	def takes_block_6 
+		prev_val = 0
+		accumulator = 0
+		self.each do |val|
+			accumulator = yield val, accumulator
+			prev_val = val
+		end
+		accumulator
+	end
+
+# Works for addition but not multiply because of zero
+	def takes_block_7 
+		previous_value = 0
+		return_value = 0
+		self.each do |value|
+			return_value = yield previous_value, value
+			previous_value = return_value
+		end
+		return_value
+	end
+
+	def takes_block_8
+		accumulator = 0
+		self.each do |value|
+			accumulator = yield accumulator, value
+		end
+	end
+
+	def takes_block_9
+		accumulator = 0
+		self.each do |value|
+			accumulator = yield accumulator, value
+		end
+		accumulator
+	end
+
+	def takes_block_10
+		accumulator = self.first
+		self.each do |value|
+			accumulator = yield accumulator, value
+		end
+		accumulator
+	end
+
+	
+	def takes_block_11
+		first, *rest = self
+		accumulator = first
+		rest.each do |value|
+			accumulator = yield accumulator, value
+		end
+		accumulator
+	end
+
+ 	def takes_block_12	
+ 		first, *rest = self
+		accumulator = yield '', first
+		rest.each do |value|
+			accumulator = yield accumulator, value
+		end
+		accumulator
+	end
+
+
+
+
+	def for_loop_practice
+		for element in self do
+  		puts element
+  	end
+	end
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- inject_method/spec/array_spec.rb
+++ inject_method/spec/array_spec.rb
@@ -44,15 +44,117 @@ describe 'reopening of Array class' do
 
 
 	it 'takes a block, logic in block' do
-		expect(array.takes_block_4{ |sum, val| sum += val }).to eq 10
+		expect(array.takes_block_4{ |sum, val| sum += val }).to eq 0
 	end
 
 
 
 
-
 	it 'takes a block 5' do
-		expect(array.takes_block_5{ |sum, val| sum + val }).to eq 34
+		expect(array.takes_block_5(3,4){ |sum, val| sum + val }).to eq 7
 	end 
 
+	
+
+	it 'takes a block 6' do
+		expect([2,3].takes_block_6{ |sum, val| sum + val }).to eq 5
+	end
+
+	it 'takes a block 7' do
+		expect([3,4,6].takes_block_7{ |sum, val| sum + val }).to eq 13
+	end
+
+	it 'takes a block 7' do
+		expect(array.takes_block_7{ |sum, val| sum + val }).to eq 10
+	end
+
+
+	it 'takes a block 8' do
+		expect(array.takes_block_8{ |sum, val| sum + val }).to eq [1,2,3,4]
+	end
+
+	it 'takes a block 9' do
+    expect(array.takes_block_9{ |sum, val| sum + val }).to eq 10
+  end  
+
+	it 'takes a block 5 with multiply' do
+		expect(array.takes_block_5(3,4){ |sum, val| sum * val }).to eq 12
+	end
+
+	it 'takes an array of length 2 with multiply' do
+		expect([5,6].takes_block_10{ |product, val| product * val }).to eq 150
+	end
+
+	it 'practices for loop and array' do
+		expect(array.for_loop_practice).to eq [1,2,3,4]
+	end
+
+	it 'takes an array of length 2 with multiply' do
+		expect([5,6].takes_block_11{ |product, val| product * val }).to eq 30
+	end
+
+	it 'can inject-multiply arrays' do
+		expect(array.takes_block_11{ |product, val| product * val }).to eq 24
+	end
+
+	it 'can inject-add arrays' do
+		expect(array.takes_block_11{ |product, val| product + val }).to eq 10
+	end
+
+	it 'can inject-interpolate strings' do
+		expect(["h", "e", "l", "l", "o"].takes_block_11{ |memo, char| memo << char }).to eq "hello"
+	end
+
+	it 'can inject-interpolate strings' do
+		expect(["h", "e", "l", "l", "o"].takes_block_11{ |memo, char| memo.concat char }).to eq "hello"
+	end
+
+	it "inject plus some other stuff - block 11" do
+		expect([65,66,67].takes_block_11 {|memo, number| memo.to_s + number.chr}).to eq '65BC'
+	end
+	
+	it "inject plus some other stuff - block 12" do
+		expect([65,66,67].takes_block_12{|memo, number| memo + number.chr}).to eq 'ABC'
+	end
+
+
+	# it 'can build a hash - block 11' do
+	# 	expect([[:first_name, 'Shane'], [:last_name, 'Harvie']].takes_block_11 { |result, element| result[element.first] = element.last; result }).to eq nil
+	# end
+
+
+	# it 'can build a hash - block 12' do
+	# 	expect([[:first_name, 'Shane'], [:last_name, 'Harvie']].takes_block_12 { |result, element| result[element.first] = element.last; result }).to eq nil
+	# end
+
+	# it 'does hash' do
+	# 	expect(hash = [[:first_name, 'Shane'], [:last_name, 'Harvie']].inject { |result, element|;result[element.first] = element.last; result }).to eq nil
+	# end
+
+
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
